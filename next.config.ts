7f9932a0f35880eaf46680924c81631d017b1afc
Abdul Wahab <163295342+Abdul0006@users.noreply.github.com>
2025-10-11 14:52:56 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,42 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value:
-      "default-src 'self'; " +
-      "script-src 'self'; " + // no inline/eval scripts
-      "object-src 'none'; " + // disallow Flash, plugins, etc.
-      "style-src 'self' https://fonts.googleapis.com; " +
-      "img-src 'self' https://*; " +
-      "font-src 'self' https://fonts.gstatic.com; " +
-      "connect-src 'self' https://*; " +
-      "frame-ancestors 'none'; " +
-      "base-uri 'self'; " +
-      "form-action 'self';",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "DENY",
-  },
-  {
-    key: "Referrer-Policy",
-    value: "strict-origin-when-cross-origin",
-  },
-  {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=(), payment=(), usb=()",
-  },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
-  },
-];
+// const securityHeaders = [
+//   {
+//     key: "Content-Security-Policy",
+//     value:
+//       "default-src 'self'; " +
+//       "script-src 'self'; " + // no inline/eval scripts
+//       "object-src 'none'; " + // disallow Flash, plugins, etc.
+//       "style-src 'self' https://fonts.googleapis.com; " +
+//       "img-src 'self' https://*; " +
+//       "font-src 'self' https://fonts.gstatic.com; " +
+//       "connect-src 'self' https://*; " +
+//       "frame-ancestors 'none'; " +
+//       "base-uri 'self'; " +
+//       "form-action 'self';",
+//   },
+//   {
+//     key: "X-Content-Type-Options",
+//     value: "nosniff",
+//   },
+//   {
+//     key: "X-Frame-Options",
+//     value: "DENY",
+//   },
+//   {
+//     key: "Referrer-Policy",
+//     value: "strict-origin-when-cross-origin",
+//   },
+//   {
+//     key: "Permissions-Policy",
+//     value: "camera=(), microphone=(), geolocation=(), payment=(), usb=()",
+//   },
+//   {
+//     key: "Strict-Transport-Security",
+//     value: "max-age=63072000; includeSubDomains; preload",
+//   },
+// ];
 
 const nextConfig: NextConfig = {
   async headers() {
